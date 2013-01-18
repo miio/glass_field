f33586013a28608847eb9d9d0c7ec3c33f647323
miio <info@miio.info>
2013-01-19 01:16:01 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassField::Application.config.secret_token = '9ccba3ec6fe9b299a866cd40b93da4ec118a57e2ec77836864f6f2a7f6372e77b0abb5a1cee2fdb000f77fbccabdfa28722752af93c0937e08517f6250755c6a'
